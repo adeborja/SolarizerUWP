6ff0842f16acb89b9905bca16f495ac58bfe5acf
Sam <samugarcia23@gmail.com>
2018-10-24 14:39:35 +0200

    agenda Terminada, cita empezada

--- 10-UWP-Solarizr/10-UWP-Solarizr/Views/agendaView.xaml.cs
+++ 10-UWP-Solarizr/10-UWP-Solarizr/Views/agendaView.xaml.cs
@@ -27,7 +27,6 @@ namespace _10_UWP_Solarizr.Views
 		public agendaView()
 		{
 			this.InitializeComponent();
-			
 		}
 
 		protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -38,8 +37,76 @@ namespace _10_UWP_Solarizr.Views
 
 			// Set the map location.
 			MapControl1.Center = cityCenter;
-			MapControl1.ZoomLevel = 16;
+			MapControl1.ZoomLevel = 15;
 			MapControl1.LandmarksVisible = true;
+			AddSpaceNeedleIcon();
+		}
+
+		public void AddSpaceNeedleIcon()
+		{
+			var MyLandmarks = new List<MapElement>();
+
+			BasicGeoposition snPosition = new BasicGeoposition { Latitude = 37.390890, Longitude = -5.973330 };
+			Geopoint snPoint = new Geopoint(snPosition);
+
+			BasicGeoposition snPosition2 = new BasicGeoposition { Latitude = 37.386983, Longitude = -5.973757 };
+			Geopoint snPoint2 = new Geopoint(snPosition2);
+			BasicGeoposition snPosition3 = new BasicGeoposition { Latitude = 37.391348, Longitude = -5.979934 };
+			Geopoint snPoint3 = new Geopoint(snPosition3);
+
+			BasicGeoposition snPosition4 = new BasicGeoposition { Latitude = 37.388443, Longitude = -5.969110 };
+			Geopoint snPoint4 = new Geopoint(snPosition4);
+
+
+			var icono1 = new MapIcon
+			{
+				Location = snPoint,
+				NormalizedAnchorPoint = new Point(0.5, 1.0),
+				ZIndex = 0,
+				Title = "Calle Nueva Orleans 12, 1ºF"
+			};
+
+			var icono2 = new MapIcon
+			{
+				Location = snPoint2,
+				NormalizedAnchorPoint = new Point(0.5, 1.0),
+				ZIndex = 0,
+				Title = "Avenida Circular 45, 3ºA"
+			};
+
+			var icono3 = new MapIcon
+			{
+				Location = snPoint3,
+				NormalizedAnchorPoint = new Point(0.5, 1.0),
+				ZIndex = 0,
+				Title = "Calle Super Nova 12, 5ºD"
+			};
+
+			var icono4 = new MapIcon
+			{
+				Location = snPoint4,
+				NormalizedAnchorPoint = new Point(0.5, 1.0),
+				ZIndex = 0,
+				Title = "Calle Asfaltada 32, Bajo B"
+			};
+
+			MyLandmarks.Add(icono1);
+			MyLandmarks.Add(icono2);
+			MyLandmarks.Add(icono3);
+			MyLandmarks.Add(icono4);
+
+
+			var LandmarksLayer = new MapElementsLayer
+			{
+				ZIndex = 1,
+				MapElements = MyLandmarks
+			};
+
+			MapControl1.Layers.Add(LandmarksLayer);
+
+			MapControl1.Center = snPoint;
+			MapControl1.ZoomLevel = 15;
+
 		}
 
 		private void rpText1_PointerPressed(object sender, PointerRoutedEventArgs e)
